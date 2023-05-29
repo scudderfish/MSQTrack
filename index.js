12cb36e8a197fb1bc81c92adb12be716410f51b3
David Smith <dave@smithfamily.org.uk>
2023-05-29 16:56:46 +0100

    Process files by timestamp order

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const fs = require('node:fs/promises');
+const fs = require('fs');
 const xml2js = require('xml2js');
 const util = require('util')
 processFiles();
 
-async function processFiles() {
+function processFiles() {
     const parser = new xml2js.Parser();
+    const dir = __dirname+"/";
+    const files = fs.readdirSync(dir);
+    const MSQFiles = files.filter(fn => fn.endsWith('.msq'));
 
-    const files =  await fs.readdir(__dirname);
-    const MSQFiles = files.filter(fn=>fn.endsWith('.msq'));
-    
-    MSQFiles.forEach(async(msq)=>{
+    MSQFiles.sort(function (a, b) {
+        return fs.statSync(dir + a).mtime.getTime() -
+            fs.statSync(dir + b).mtime.getTime();
+    });
 
-        const contents = await fs.readFile(__dirname + "/" + msq);
+
+    MSQFiles.forEach((msq) => {
+
+        const contents = fs.readFileSync(dir + msq);
 
         parser.parseString(contents, function (err, result) {
             const veTableText = result.msq.page[2].constant[0]._;
             const values = processVeText(veTableText);
-            console.log(msq,',',util.inspect(values,{maxArrayLength:256,compact:true, breakLength:10000}));
+            let data = msq;
+            values.forEach(v => data = data + "\t" + v);
+            console.log(data);
         });
-    
+
 
     })
 }
 function processVeText(text) {
-    let values=[];
+    let values = [];
     const lines = text.split('\n').filter(s => s.trim().length > 3).forEach(l => {
         l = l.trim();
         const data = l.split(' ').map(n => Number(n));
-        values=values.concat(data);
+        values = values.concat(data);
     });
     return values;
 }
